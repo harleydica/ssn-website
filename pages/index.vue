86b278069e2192af52b91b32332a312263c8af4d
harleydica <taufiklah87@gmail.com>
2023-07-07 19:00:00 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header></Header>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/registrasi.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Ini adalah halaman registrasi</h1>
+        <p>Ini adalah paragraf</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RegistrasiPage'
+}
+</script>
